70cb6bb065f4835cb73dd1cff938c82cb3815fd3
kazuyasu aoki <59601271+aokazu55@users.noreply.github.com>
2020-01-23 16:08:22 +0900

    Add files via upload

---
+++ clean/bs.rb
@@ -0,0 +1,35 @@
+a = [1,7,3,4,2,2,6,5,7,8,0,1,8]
+b = [2,6,5,7,8,1,7,3,4,2,0,1,8,4,9,9,2,6,3]
+c = [1,8,4,9,5,7,8,4,2,0]
+# 簡単な方の回答
+def bucket_sort(arr)
+  # 値を入れるバケツと、ソートした値を入れ直すバケツを二つ用意する
+  bucket = []
+  sorted_bucket = []
+  max_number = 0
+  # 配列のインデックス番号を調整
+  number_adjustment = 1
+  arr.each do |number|
+    if number > max_number
+      max_number = number
+    end
+    max_number = max_number + number_adjustment
+  end
+  # 与えられた配列の要素の数だけ、バケツの容量を用意する
+  max_number.times do
+    bucket << []
+  end
+  # 値を一つずつバケツに入れる。バケツのindex番号と一致する箇所に値を入れる
+  arr.each do |i|
+    bucket[i] << i
+  end
+  # ソートされた値をバケツに返していく
+  bucket.each do |e|
+    sorted_bucket = sorted_bucket + e
+  end
+  # ソートされたバケツを返り値として返却する
+  sorted_bucket
+end
+p bucket_sort(a)
+p bucket_sort(b)
+p bucket_sort(c)
---
+++ clean/play.rb
@@ -0,0 +1,7 @@
+def introduce(name, weather)
+  puts "おはようございます"
+  puts "#{name}です！今日も頑張っていきましょう！"
+  puts "気になる今日の天気ですが、ズバリ「#{weather}」です。}"
+
+end
+introduce("青木", "晴れ")
